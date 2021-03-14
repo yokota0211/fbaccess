9b9013d77cc8f460c9c62a275640d0394079a121
hishinuma616 <dnbwoodleaves3616@gmail.com>
2021-03-14 10:05:59 +0900

    getData修正

--- pages/fb2.vue
+++ pages/fb2.vue
@@ -1,13 +1,15 @@
 <template>
-<section>
-<h1>{{title}}</h1>
-<p>{{message}}</p>
-<input v-model="find">
-<button @click="getData">Click</button>
-<hr>
-<ul v-for="(data, key) in json_data">
- <li>{{data.name}} ({{data.tel}}) [{{key}}]</li>
-</ul>
+  <section>
+  <h1>{{ title }}</h1>
+  <p>{{ message }}</p>
+  <input v-model="value">
+  <button @click="getData">Click</button>
+  <hr>
+  <ul v-for="(data, key) in json_data" :key="key">
+    <li>
+      {{ data.name }} ({{ data.tel }}) [{{ key }}]
+    </li>
+  </ul>
 </section>
 
 </template>
@@ -23,52 +25,34 @@ const axios = require('axios');
 //https://test1226-5d13e-default-rtdb.firebaseio.com/employee.json?print=pretty
 //参考：https://firebase.google.com/docs/database/rest/retrieve-data?hl=ja
 
-let url = "https://testfirebase-a7a2e-default-rtdb.firebaseio.com/student.json?orderBy=%22$key%22&equalTo=%22";
+// https://testfirebase-a7a2e-default-rtdb.firebaseio.com/student.json?orderBy=key&equalTo=0001
 
 export default {
-    data: function(){
-        return {
-            title:'Axios',
-            message:'axios sample.',
-            //json_data:{},
-        };
-    },
-    methods: {
-
- /* 実行時に結果が表示されない...
+  data() {
+    return {
+        title:'Axios',
+        message:'axios sample.',
+        json_data: {},
+        value: '',
+    };
+  },
+  methods: {
     getData() {
-
-        const url = "https://testfirebase-a7a2e-default-rtdb.firebaseio.com/student.json";
-
-        axios.get(url, {
-         params:{
-          orderBy: key,
-          equalTo: this.find,
-         }   
-        }).then((res) => {
-            this.message = 'get ID=' + this.find;
-            this.json_data = res.data;
-            console.log(res);
-        }).catch((error) =>{
-            this.message = 'EROR!';
-           // this.json_data = {};
-        });
+      const url = 'https://testfirebase-a7a2e-default-rtdb.firebaseio.com/student.json';
+      axios.get(url, {
+        params: {
+        orderBy: `"$key"`,
+        equalTo: `"${this.value}"`,
+        }
+      }).then(res => {
+        this.message = `get ID=${this.value}`;
+        this.json_data = res.data;
+        console.log(res);
+      }).catch(error => {
+        this.message = 'EROR!';
+        console.error(error);
+      });
     },
-*/
-        getData: function(){
-
-         let r_url = url + this.find + '%22';
-
-         axios.get(r_url).then((res) => {
-            this.message = 'get ID=' + this.find;
-            this.json_data = res.data;
-         }).catch((error) =>{
-            this.message = 'EROR!';
-            this.json_data = {};
-         });
-     }
-
-
-   }
+  },
 }
 </script>
